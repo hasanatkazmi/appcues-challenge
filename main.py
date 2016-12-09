bac926b7e3b50bad3d0d1fb8c3a3180eb63a36d4
Hasanat Kazmi <hasanatkazmi@gmail.com>
2016-12-09 01:51:56 -0800

    more implementation

--- main.py
+++ main.py
@@ -1,7 +1,124 @@
 #!/usr/bin/env python
 
+import time
+import sqlite3
+import os
+import copy
 import BaseHTTPServer
 from urlparse import parse_qs
+from threading import Lock, Thread
+from functools import wraps
+
+CACHE_SIZE = 1000000  # 1 MB
+AGE_TRIGGER = 8  # 8 sec
+
+
+def run_async(func):
+    @wraps(func)
+    def async_func(*args, **kwargs):
+        func_hl = Thread(target=func, args=args, kwargs=kwargs)
+        func_hl.start()
+        return func_hl
+
+    return async_func
+
+
+shutdown_hook=False
+
+class DbManager:
+    def __init__(self, cache, db_path='numbers.db'):
+        self.db_path = db_path
+        if self.is_db():
+            raw_input('Path {path} is not empty, enter any key to remove it'.format(path=db_path))
+            self.rm_db()
+        self.cache = cache
+        self.cache.register_expiry_callback(self.flush)
+
+    def create_db(self):
+        self.conn = sqlite3.connect(self.db_path)
+        self.cur = self.conn.cursor()
+        self.cur.execute('''CREATE TABLE numbers (key TEXT, value INTEGER DEFAULT 0);''')
+        self.cur.execute('''CREATE UNIQUE INDEX numbers_key_index ON numbers (key);''')
+        self.conn.commit()
+
+    def is_db(self):
+        return os.path.isdir(self.db_path) or os.path.isfile(self.db_path)
+
+    def rm_db(self):
+        os.remove(self.db_path)
+
+    def close(self):
+        self.conn.close()
+
+    def flush(self):
+        '''write cache to db'''
+        self.is_db() or self.create_db()
+        sql_stmp = self.cache.flush()
+        print sql_stmp
+        if sql_stmp:
+            self.cur.execute(sql_stmp)
+            self.conn.commit()
+
+        global shutdown_hook
+        if shutdown_hook:
+            self.close()
+
+
+class InMemStore:
+    key_val_store = {}
+    store_lock = Lock()
+    mem_size = 0
+    age = time.time()
+
+    def __init__(self):
+        pass
+
+    def put(self, key, val):
+        if key in self.key_val_store:
+            self.mem_size -= len(key) - len(str(self.key_val_store[key]))
+        self.store_lock.acquire()
+        try:
+            self.key_val_store[key] = val
+        finally:
+            self.store_lock.release()
+        self.mem_size += len(key) + len(str(val))
+
+    def get_size(self):
+        return self.mem_size
+
+    def flush(self):
+        self.store_lock.acquire()
+        try:
+            # tmp_store = copy.copy(self.key_val_store)
+            tmp_store = self.key_val_store
+
+            self.key_val_store = {}
+            self.age = time.time()
+            self.mem_size = 0
+        finally:
+            self.store_lock.release()
+        sql_stmt = InMemStore.get_store_as_sql(tmp_store)
+        return sql_stmt
+
+    def get_age(self):
+        return time.time() - self.age
+
+    @staticmethod
+    def get_store_as_sql(store):
+        stmt = '''INSERT OR REPLACE INTO 'numbers' ('key', 'value') VALUES {tuples} ;'''
+        tuples = ','.join(["('"+k+"', "+str(v)+")" for k, v in store.iteritems()])
+        return None if store == {} else stmt.format(tuples=tuples)
+
+    @run_async
+    def register_expiry_callback(self, func):
+        global shutdown_hook
+        while True:
+            if self.get_age() > AGE_TRIGGER or self.get_size() >= CACHE_SIZE or shutdown_hook:
+                func()
+            if shutdown_hook:
+                break
+            time.sleep(1)
+
 
 class AppcuesServer(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
@@ -22,10 +139,20 @@ class AppcuesServer(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_response(400)
             return
         key, value = content['key'][0], content['value'][0]
-        value = int(value)
-        print key, value
-        self.send_response(200)
+        try:
+            value = int(value)
+        except ValueError:
+            self.send_response(400)
+            return
+        # print key, value
+        self.store.put(key, value)
+        self.send_response(200)       
 
+    def log_message(self, format, *args):
+        return
+
+    store = InMemStore()
+    db = DbManager(store)
 
 if __name__ == '__main__':
     server_port = 3333
@@ -36,3 +163,6 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print "Shutting down the server"
         server.socket.close()
+        shutdown_hook = True
+        server.shutdown()
+
